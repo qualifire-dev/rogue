41f6fbb5c387dcb59e40f836b8056aafa1d7c5b2
Yuval <yuval@qualifire.ai>
2025-06-18 23:45:45 +0300

    Forgot to add evaluation results model file

---
+++ agent_evaluator/models/evaluation_result.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+from .chat_history import ChatHistory
+from .scenario import Scenario
+
+
+class ConversationEvaluation(BaseModel):
+    messages: ChatHistory
+    passed: bool
+    reason: str
+
+
+class EvaluationResult(BaseModel):
+    scenario: Scenario
+    conversations: List[ConversationEvaluation]
+    passed: bool
+
+
+class EvaluationResults(BaseModel):
+    results: List[EvaluationResult] = Field(default_factory=list)
+
+    def add_result(self, new_result: EvaluationResult):
+        for result in self.results:
+            if result.scenario.scenario == new_result.scenario.scenario:
+                result.conversations.extend(new_result.conversations)
+                result.passed = result.passed and new_result.passed
+                return
+        self.results.append(new_result)
+
+    @classmethod
+    def combine(
+        cls, *results: "EvaluationResults" | List["EvaluationResults"] | None
+    ) -> "EvaluationResults":
+        combined = EvaluationResults()
+        for evaluation_result in results:
+            if evaluation_result is None:
+                continue
+
+            if isinstance(evaluation_result, list):
+                evaluation_result = cls.combine(*evaluation_result)
+
+            combined.results.extend(evaluation_result.results)
+        return combined
