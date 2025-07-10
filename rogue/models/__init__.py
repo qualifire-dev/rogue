395bcc582e64247e37a3f4510dcfd659f2e18d02
yuval-qf <yuval@qualifire.ai>
2025-07-10 09:45:05 +0300

    Add tests (#12)
    * Clean code
    
    * Add models/config tests
    
    * Add models/cli_input tests
    
    * Add models/cli_input tests
    
    * Add models/scenario tests
    
    * Add models/evaluation_result tests
    
    ---------
    
    Co-authored-by: drorIvry <drorIvry@gmail.com>

--- rogue/models/__init__.py
+++ rogue/models/__init__.py
@@ -1,3 +1,6 @@
 from . import chat_history
+from . import cli_input
+from . import config
+from . import evaluation_result
+from . import prompt_injection
 from . import scenario
-from . import user_config
---
+++ rogue/tests/models/test_evaluation_result.py
@@ -0,0 +1,161 @@
+import pytest
+
+from rogue.models.chat_history import ChatHistory, Message
+from rogue.models.evaluation_result import (
+    EvaluationResults,
+    ConversationEvaluation,
+    EvaluationResult,
+)
+from rogue.models.scenario import Scenario
+
+
+class TestEvaluationResults:
+    scenario_1 = Scenario(scenario="Scenario 1")
+    scenario_2 = Scenario(scenario="Scenario 2")
+
+    conversation_1_passed = ConversationEvaluation(
+        messages=ChatHistory(messages=[Message(role="user", content="message 1")]),
+        passed=True,
+        reason="reason",
+    )
+    conversation_2_passed = ConversationEvaluation(
+        messages=ChatHistory(messages=[Message(role="user", content="message 2")]),
+        passed=True,
+        reason="reason",
+    )
+
+    conversation_1_failed = ConversationEvaluation(
+        messages=ChatHistory(messages=[Message(role="user", content="message 1")]),
+        passed=False,
+        reason="reason",
+    )
+    conversation_2_failed = ConversationEvaluation(
+        messages=ChatHistory(messages=[Message(role="user", content="message 2")]),
+        passed=False,
+        reason="reason",
+    )
+
+    @staticmethod
+    def get_evaluation_result(
+        scenario: Scenario,
+        conversation: ConversationEvaluation,
+    ) -> EvaluationResult:
+        return EvaluationResult(
+            scenario=scenario,
+            conversations=[conversation],
+            passed=conversation.passed,
+        )
+
+    @pytest.mark.parametrize(
+        "existing_results, new_result, expected_results",
+        [
+            # no overlap from empty results
+            (
+                EvaluationResults(),
+                get_evaluation_result(scenario_1, conversation_1_passed),
+                EvaluationResults(
+                    results=[get_evaluation_result(scenario_1, conversation_1_passed)]
+                ),
+            ),
+            # no overlap from non-empty results
+            (
+                EvaluationResults(
+                    results=[get_evaluation_result(scenario_1, conversation_1_passed)]
+                ),
+                get_evaluation_result(scenario_2, conversation_1_failed),
+                EvaluationResults(
+                    results=[
+                        get_evaluation_result(scenario_1, conversation_1_passed),
+                        get_evaluation_result(scenario_2, conversation_1_failed),
+                    ]
+                ),
+            ),
+            # scenario overlap with passed unchanged True -> True
+            (
+                EvaluationResults(
+                    results=[get_evaluation_result(scenario_1, conversation_1_passed)]
+                ),
+                get_evaluation_result(scenario_1, conversation_2_passed),
+                EvaluationResults(
+                    results=[
+                        EvaluationResult(
+                            scenario=scenario_1,
+                            conversations=[
+                                conversation_1_passed,
+                                conversation_2_passed,
+                            ],
+                            passed=True,
+                        ),
+                    ]
+                ),
+            ),
+            # scenario overlap with passed changed True -> False
+            (
+                EvaluationResults(
+                    results=[get_evaluation_result(scenario_1, conversation_1_passed)]
+                ),
+                get_evaluation_result(scenario_1, conversation_2_failed),
+                EvaluationResults(
+                    results=[
+                        EvaluationResult(
+                            scenario=scenario_1,
+                            conversations=[
+                                conversation_1_passed,
+                                conversation_2_failed,
+                            ],
+                            passed=False,
+                        ),
+                    ]
+                ),
+            ),
+            # scenario overlap with passed unchanged False -> False (#1)
+            (
+                EvaluationResults(
+                    results=[get_evaluation_result(scenario_1, conversation_1_failed)]
+                ),
+                get_evaluation_result(scenario_1, conversation_2_failed),
+                EvaluationResults(
+                    results=[
+                        EvaluationResult(
+                            scenario=scenario_1,
+                            conversations=[
+                                conversation_1_failed,
+                                conversation_2_failed,
+                            ],
+                            passed=False,
+                        ),
+                    ]
+                ),
+            ),
+            # scenario overlap with passed unchanged False -> False (#2)
+            (
+                EvaluationResults(
+                    results=[get_evaluation_result(scenario_1, conversation_1_failed)]
+                ),
+                get_evaluation_result(
+                    scenario_1,
+                    conversation_2_passed,
+                ),  # passed is ignored here because we already failed
+                EvaluationResults(
+                    results=[
+                        EvaluationResult(
+                            scenario=scenario_1,
+                            conversations=[
+                                conversation_1_failed,
+                                conversation_2_passed,
+                            ],
+                            passed=False,
+                        ),
+                    ]
+                ),
+            ),
+        ],
+    )
+    def test_add_result(
+        self,
+        existing_results: EvaluationResults,
+        new_result: EvaluationResult,
+        expected_results: EvaluationResults,
+    ):
+        existing_results.add_result(new_result)
+        assert existing_results == expected_results
